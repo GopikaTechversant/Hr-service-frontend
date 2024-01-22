f084911ca030132c3da201492c9cf66224f42755
Jyothi <jyothi@zoftsolutions.com>
2024-01-22 10:35:43 +0530

    changed api url

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     prodEnv: false,
     devEnv: true,
-    api_url : 'http://ec2-52-195-172-72.ap-northeast-1.compute.amazonaws.com:3001'
+    api_url : 'http://ec2-35-78-94-26.ap-northeast-1.compute.amazonaws.com:3001'
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -10,6 +10,6 @@ export const environment = {
     production: false,
     prodEnv: false,
     devEnv: true,
-    api_url : 'http://ec2-52-195-172-72.ap-northeast-1.compute.amazonaws.com:3001'
+    api_url : 'http://ec2-35-78-94-26.ap-northeast-1.compute.amazonaws.com:3001'
 
 };
