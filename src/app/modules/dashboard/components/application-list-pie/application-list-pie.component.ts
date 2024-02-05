61be6c95fcb4aaedb7d9125ad55ede0f2631a2e7
Alfiya S R <alfiya.sr@techversantinfotech.com>
2024-02-05 18:09:43 +0530

    dashboard layout

--- src/app/modules/dashboard/components/application-list-pie/application-list-pie.component.ts
+++ src/app/modules/dashboard/components/application-list-pie/application-list-pie.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-application-list-pie',
   templateUrl: './application-list-pie.component.html',
   styleUrls: ['./application-list-pie.component.css']
 })
-export class ApplicationListPieComponent {
+export class ApplicationListPieComponent implements OnInit {
+  chart: any;
 
+  ngOnInit(): void {
+    this.createChart(); 
+  }
+
+  createChart() {
+    this.chart = new Chart("MyChart", {
+      type: 'doughnut',
+      data: {
+        labels: ['Naukiri', 'Indeed', 'LinkedIn'],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [274, 17, 31],
+          backgroundColor: [
+            '#628afc',
+            '#005ec9',
+            '#047892',     
+          ],
+          hoverOffset: 4
+        }],
+      },
+      options: {
+        aspectRatio: 1.9
+      }
+    });
+  }
 }
