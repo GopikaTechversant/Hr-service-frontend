da9707e37d13403369dcc49cc75bf5318b58e2c1
Alfiya S R <alfiya.sr@techversantinfotech.com>
2024-02-06 14:24:36 +0530

    charts

--- src/app/modules/dashboard/components/application-list-bar/application-list-bar.component.ts
+++ src/app/modules/dashboard/components/application-list-bar/application-list-bar.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-application-list-bar',
   templateUrl: './application-list-bar.component.html',
   styleUrls: ['./application-list-bar.component.css']
 })
-export class ApplicationListBarComponent {
+export class ApplicationListBarComponent implements OnInit {
+  chart: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.createBarChart();
+  }
+
+  createBarChart() {
+    this.chart = new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: ['SE/SSE- Java', 'SE/SSE - Node JS', 'TL - .NET ', 'SE/SSE- PHP', 'SE/SSE- DBA', 'TL - Automation'],
+        datasets: [{
+          label: 'Applicants',
+          data: [0, 5, 10, 15, 25],
+          backgroundColor: [
+            'rgba(98, 138, 252)',
+            'rgba(98, 138, 252)',
+            'rgba(98, 138, 252)',
+            'rgba(98, 138, 252)',
+            'rgba(98, 138, 252)',
+            'rgba(98, 138, 252)',
+          ],
+          borderColor: [
+            'rgba(98, 138, 252)',
+            'rgba(98, 138, 252)',
+            'rgba(98, 138, 252)',
+            'rgba(98, 138, 252)',
+            'rgba(98, 138, 252)',
+            'rgba(98, 138, 252)',
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            grid: {
+              display: false, 
+            }
+          },
+          x: {
+            grid: {
+              display: false,
+            }
+          }
+        }
+      }
+    });
+  }
+  
 
 }
--- src/app/modules/dashboard/components/application-list-pie/application-list-pie.component.ts
+++ src/app/modules/dashboard/components/application-list-pie/application-list-pie.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js/auto';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 
 @Component({
@@ -11,28 +12,54 @@ export class ApplicationListPieComponent implements OnInit {
   chart: any;
 
   ngOnInit(): void {
+    Chart.register(ChartDataLabels);
+
     this.createChart(); 
   }
-
   createChart() {
     this.chart = new Chart("MyChart", {
       type: 'doughnut',
       data: {
         labels: ['Naukiri', 'Indeed', 'LinkedIn'],
         datasets: [{
-          label: 'My First Dataset',
+          label: 'Applicants',
           data: [274, 17, 31],
           backgroundColor: [
             '#628afc',
             '#005ec9',
             '#047892',     
           ],
-          hoverOffset: 4
+          // hoverOffset: 4
         }],
       },
       options: {
-        aspectRatio: 1.9
-      }
+        aspectRatio: 1.7,
+        plugins: {
+          datalabels: {
+            color: '#575F6E', 
+            backgroundColor: '#edeff5',
+            borderRadius: 4, 
+            borderColor: '#ffffff', 
+            borderWidth: 1, 
+            padding: 4, 
+            anchor: 'end', 
+            align: 'end', 
+            offset: -10, 
+            font: {
+              size: 10, 
+              weight: 'bold',
+            },
+            formatter: (value, context) => {
+              if (context.chart.data.labels !== undefined) {
+                return context.chart.data.labels[context.dataIndex] + ': ' + value;
+              }
+              return 'Unknown: ' + value;
+            },
+          },
+        },
+      },  
+      plugins: [ChartDataLabels], 
     });
   }
+  
 }
