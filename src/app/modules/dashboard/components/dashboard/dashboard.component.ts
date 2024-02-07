7c78563fc47010775758c6838e77702e443c04d8
Alfiya S R <alfiya.sr@techversantinfotech.com>
2024-02-07 17:47:08 +0530

    list candidate

--- src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,35 +1,41 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
   candidates: any;
   list = [
-    { count : 250 ,
-      name: 'Total Applicants', 
-      position :'Senior Software Engineer',
+    {
+      count: 250,
+      name: 'Total Applicants',
+      position: 'Senior Software Engineer',
       department: 'Javascript',
     },
-    { count : 50 ,
-      name: 'Shortlisted Candidates', 
-      position :'Senior Software Engineer',
+    {
+      count: 50,
+      name: 'Shortlisted Candidates',
+      position: 'Senior Software Engineer',
       department: 'Javascript',
     },
-    { count : 20 ,
-      name: 'Hired Candidates', 
-      position :'Senior Software Engineer',
+    {
+      count: 20,
+      name: 'Hired Candidates',
+      position: 'Senior Software Engineer',
       department: 'Javascript',
-    },{ count : 100 ,
-      name: 'Rejected Candidates', 
-      position :'Senior Software Engineer',
+    }, {
+      count: 100,
+      name: 'Rejected Candidates',
+      position: 'Senior Software Engineer',
       department: 'Javascript',
     }
   ];
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
-  
+
   }
+
+
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -3,6 +3,6 @@ export const environment = {
     production: false,
     prodEnv: false,
     devEnv: true,
-    api_url: 'http://ec2-35-78-94-26.ap-northeast-1.compute.amazonaws.com:3001'
+    api_url: 'http://ec2-18-183-153-204.ap-northeast-1.compute.amazonaws.com:3001'
 
 };
