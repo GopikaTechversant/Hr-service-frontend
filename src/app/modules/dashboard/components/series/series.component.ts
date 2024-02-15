4954165d59315fb2e4e245a8d6cb417ca8d4164a
GopikaTechversant <gopikakg@techversantinfotech.com>
2024-02-15 18:18:36 +0530

    add station flow,ui changes

--- src/app/modules/dashboard/components/series/series.component.ts
+++ src/app/modules/dashboard/components/series/series.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environments';
 import { ActivatedRoute } from '@angular/router';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { ApprovalStatusService } from 'src/app/services/approval-status.service';
+import { FeedbackComponent } from 'src/app/components/feedback/feedback.component';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-series',
   templateUrl: './series.component.html',
@@ -18,7 +20,8 @@ import { ApprovalStatusService } from 'src/app/services/approval-status.service'
   ],
 })
 export class SeriesComponent implements OnInit {
-  // series_list: any = [{ name: 'Series1' }]
+  @Input() rejectedCandidates: string[] = [];
+  @Input() selectedCandidatesIds: string[] = [];
   series_list: any = [];
   activeSeries: any;
   candidates_list: any;
@@ -35,115 +38,66 @@ export class SeriesComponent implements OnInit {
   isTaskDetailsOpen: boolean = false;
   serviceIds: any = [];
   approvedServiceId: any;
-  constructor(private http: HttpClient, private route: ActivatedRoute,private approvalStatusService: ApprovalStatusService) {
-
+  candidateServiceId: any;
+  constructor(private http: HttpClient, private route: ActivatedRoute, private dialog: MatDialog) {
     this.route.queryParams.subscribe(params => {
       this.requestId = params['requestId'];
-      console.log(" this.requestId", this.requestId);
-
-
     });
   }
   ngOnInit(): void {
     this.fetchcandidates();
-    // console.log("this.series_list before", this.series_list);
   }
   fetchcandidates(): void {
     this.http.get(`${environment.api_url}/screening-station/list-batch/${this.requestId}`).subscribe((res: any) => {
-      // console.log("fetch candidates", res);
       this.candidates_list = res.candidates;
       this.candidates = this.candidates.candidate;
-      // console.log("this.candidates_list", this.candidates_list);
       this.candidates_list.forEach((candidate: any) => {
         if (candidate.serviceId) {
           this.serviceIds.push(candidate.serviceId);
         }
-        // console.log("Service IDs:", this.serviceIds);
       });
-
     })
   }
   seriesBoxClick(series: any) {
     this.series_list.forEach((s: any) => s.active = false);
     series.active = true;
     this.activeSeries = series;
-
   }
   allowDrop(ev: any) {
     ev.preventDefault();
   }
   onDrop(event: any) {
-    // console.log("hhfudghfui");
-
     this.selectedCandidate = event.item.data;
-    // console.log("this.selectedCandidate", this.selectedCandidate);
-
   }
   moved(event: any) {
-    // console.log("entering pointer position");
-
     this.pointerPosition = event.pointerPosition;
-    // console.log(event.pointerPosition);
   }
-  itemDropped(event: any, series: any) {
-
-    // console.log("item dropped");
-
-  }
-
   createSeries(): void {
-
-    // const newSeriesName = this.series_list.name++;
     const newSeriesName = `Series${this.series_list.length + 1}`;
-    // console.log("newSeriesName", newSeriesName);
-
     const newSeries = { name: newSeriesName };
-    // console.log("newSeries", newSeries);
-
     this.series_list.push(newSeries);
-    // console.log("after this.series_list", this.series_list);
-
     this.activeSeries = newSeries;
   }
   dragStart(event: any, candidate: any) {
-    // console.log("Drag Start");
     event.dataTransfer.setData('text/plain', JSON.stringify(candidate));
-
   }
-
   dragOver(event: DragEvent) {
-    // console.log("Drag Over Series");
     event.preventDefault();
   }
-
   productDrop(event: any, series: any) {
-    // console.log("Product Drop Series");
     event.preventDefault();
     const candidateData = event.dataTransfer.getData('text/plain');
     const candidate = JSON.parse(candidateData);
-
-    // Remove the candidate from its previous series
     this.series_list.forEach((s: any) => {
       if (s?.candidates) {
         s.candidates = s.candidates.filter((c: any) => c?.candidateId !== candidate?.candidateId);
       }
     });
-
-    // Remove the candidate from the candidates_list
     this.candidates_list = this.candidates_list.filter((c: any) => c?.candidateId !== candidate?.candidateId);
-
-    // Add the dropped candidate to the candidates array of the new series
     series.candidates = series.candidates || [];
     series.candidates.push(candidate);
-
-    // Update the series_list
     this.series_list = [...this.series_list];
-    // console.log("Candidate dropped:", candidate);
-    // console.log("Dropped into Series:", series);
-    // console.log("Series with Candidates:", this.series_list);
   }
-
-  // Handle the dragover event for the series boxes
   dragOverSeries(event: DragEvent, series: any) {
     event.preventDefault();
     this.dragEnteredSeries = series;
@@ -154,28 +108,34 @@ export class SeriesComponent implements OnInit {
       'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEyLCJ1c2VyVHlwZSI6ImFkbWluIiwidXNlckVtYWlsIjoiYWRtaW5AbWFpbGluYXRvci5jb20ifQ.Uva57Y4MMA0yWz-BYcRD-5Zzth132GMGJkFVQA3Tn50'
     });
     const requestData = {
-      serviceIds: this.serviceIds
+      serviceIds: this.selectedCandidatesIds.length > 0 ? this.selectedCandidatesIds : this.serviceIds
     }
-    this.http.post(`${environment.api_url}/screening-station/accept`, requestData,{headers}).subscribe((res: any) => {
-      // console.log("approve seriies", res);
-      alert("Approved")
-      this.approvalStatusService.updateapprovalStatus(true);
-
-    },
-    (error) => {
-      // console.log("error in approving candidates");
-      this.approvalStatusService.updateapprovalStatus(false);
-      
+    this.http.post(`${environment.api_url}/screening-station/accept`, requestData, { headers }).subscribe((res: any) => {
+      alert("approved");
     }
     )
-
   }
-
   toggleTaskDetails() {
-    // if (this.isTaskDetailsOpen) {
     this.isTaskDetailsOpen = !this.isTaskDetailsOpen;
-    // } else {
-    // this.isTaskDetailsOpen = !this.isTaskDetailsOpen;
-    // }
+  }
+  onCandidateSelectionChange(event: any, candidate: any, index: any): void {
+    let action = event?.target?.value;
+    this.candidateServiceId = candidate?.serviceId;
+    const dialogRef = this.dialog.open(FeedbackComponent, {
+      data: { candidateId: candidate?.serviceId, stationId: 1, status: action },
+      width: '400px',
+      height: '200px'
+    })
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        candidate.serviceStatus = action;
+      }
+      console.log(candidate)
+      let element: any = document.getElementById('status' + index);
+      if (element) element.value = candidate.serviceStatus;
+    })
+    dialogRef.componentInstance.selectedCandidatesEmitter.subscribe((selectedCandidatesIds: any[]) => {
+      this.selectedCandidatesIds.push(...selectedCandidatesIds);
+    })
   }
 }
