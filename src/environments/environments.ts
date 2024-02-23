6983f3ed2784138e00e4803cd8013660fe09f961
Alfiya S R <alfiya.sr@techversantinfotech.com>
2024-02-23 11:56:33 +0530

    url changed

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     prodEnv: false,
     devEnv: true,
-    api_url: 'http://ec2-18-183-153-204.ap-northeast-1.compute.amazonaws.com:3001'
+    api_url: 'http://10.1.0.73:3001'
 };
