e49b7496122fc70361cab97d3f17b331292213be
GopikaTechversant <gopikakg@techversantinfotech.com>
2024-02-29 11:15:11 +0530

    bug fixed

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -17,10 +17,20 @@ export class FeedbackComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<FeedbackComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private http: HttpClient) {
 
   }
+  
   ngOnInit(): void {
 
   }
+
   onSubmitClick(): void {
+    console.log("syyyyyy");
+    this.dialogRef.close(true);
+    this.apiCall();
+  }
+
+  apiCall():void {
+    console.log("apii call");
+    
     const feedback = document.getElementById('feedback') as HTMLInputElement;
     if (feedback) this.feedback = feedback.value;
     this.candidateServiceId = this.data.candidateId;
@@ -29,21 +39,20 @@ export class FeedbackComponent implements OnInit {
     const payload = {
       serviceId: this.candidateServiceId,
       stationId: this.stationId,
-      status: this.data.status == 'pending' ? 'selected' : 'rejected',
+      status: this.data?.status == 'pending' ? 'selected' : 'rejected',
       feedBack: this.feedback
     }
-
     this.http.post(`${environment.api_url}/screening-station/reject/candidate`, payload).subscribe((res: any) => {
-      // this.dialogRef.close(true);
-      if (this.data.status === 'pending') {
-        console.log(this.candidateServiceId);
+      console.log('checkkk');
+      if (this.data?.status === 'pending') {
+        console.log("apiiii");
         this.selectedCandidatesEmitter.emit([this.candidateServiceId]);
       }
-      this.dialogRef.close(true);
     })
-    
   }
+
   onCancelClick(): void {
     this.dialogRef.close(false);
   }
+
 }
