d916c0491e960e3de71489657e59dfa82edda360
GopikaTechversant <gopikakg@techversantinfotech.com>
2024-04-30 18:57:19 +0530

    changes in s3 service

--- src/app/services/s3.service.ts
+++ src/app/services/s3.service.ts
@@ -1,29 +1,31 @@
-import { Injectable } from '@angular/core';
-// import * as AWS from 'aws-sdk';
+import { Injectable } from "@angular/core";
+import { S3Client, PutObjectCommand, PutObjectCommandInput } from "@aws-sdk/client-s3";
+
 @Injectable({
   providedIn: 'root'
 })
 export class S3Service {
-  // private s3: AWS.S3;
+  private s3Client: S3Client;
 
-  // constructor() {
-  //   // Load AWS configuration from the JSON file
-  //   AWS.config.loadFromPath('src/assets/aws-config.json');
-  //   this.s3 = new AWS.S3();
-  // }
+  constructor() {
+    // Initialize AWS S3 client
+    this.s3Client = new S3Client({
+      region: 'your-region', // Specify your AWS region
+      credentials: {
+        accessKeyId: 'your-access-key-id', // Specify your AWS access key ID
+        secretAccessKey: 'your-secret-access-key' // Specify your AWS secret access key
+      }
+    });
+  }
 
-  // uploadImage(file: File, bucketName: string, key: string): Promise<any> {
-  //   const params = {
-  //     Bucket: bucketName,
-  //     Key: key,
-  //     Body: file,
-  //     ACL: 'public-read' // Set ACL to make the uploaded image publicly accessible
-  //   };
-  //   return new Promise((resolve, reject) => {
-  //     this.s3.upload(params, (err: any, data: any) => {
-  //       if (err) reject(err);
-  //       else resolve(data);
-  //     });
-  //   });
-  // }
+  async uploadImage(file: File, bucketName: string, key: string): Promise<any> {
+    const params: PutObjectCommandInput = {
+      Bucket: bucketName,
+      Key: key,
+      Body: file,
+      ACL: 'public-read' // Set ACL to make the uploaded image publicly accessible
+    };
+    const command = new PutObjectCommand(params);
+    return await this.s3Client.send(command);
+  }
 }
