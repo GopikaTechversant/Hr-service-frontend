2cd64eecc5438807d524ddeb98442a43dbf8efd1
Alfiya S R <alfiya.sr@techversantinfotech.com>
2024-05-03 13:00:19 +0530

    Remove environment directories from Git tracking

--- src/environments/environments.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    prodEnv: true,
-    devEnv: false,
-    api_url : 'http://ec2-18-216-125-8.us-east-2.compute.amazonaws.com:3001'
-}
--- src/environments/environments.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    prodEnv: false,
-    devEnv: true,
-    api_url: 'http://10.1.0.83:3001'
-};
