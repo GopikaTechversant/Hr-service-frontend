33234df24959c8b24a6f4d2a6ef64e8e7359aa29
Alfiya S R <alfiya.sr@techversantinfotech.com>
2024-05-14 10:41:32 +0530

    api service

--- src/environments/environments.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    prodEnv: true,
-    devEnv: false,
-    api_url : 'https://9cbb-202-88-236-230.ngrok-free.app',
-    secretKey:'yG9Z5/HpcfVMaFG1epzqvMBoLx4j6ha9L31L2tsR',
-    accessKeyId:'AKIAVWRJX4ZISZ4UO7FR'
-}
--- src/environments/environments.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    prodEnv: false,
-    devEnv: true,
-    api_url: 'https://9cbb-202-88-236-230.ngrok-free.app',
-    secretKey:'yG9Z5/HpcfVMaFG1epzqvMBoLx4j6ha9L31L2tsR',
-    accessKeyId:'AKIAVWRJX4ZISZ4UO7FR',
-    'ngrok-skip-browser-warning': 'true' 
-};
