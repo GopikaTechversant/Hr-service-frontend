e40093ecd1a70765aac65a0d0d80b3bf9cdd18d0
Alfiya S R <alfiyasr7@gmail.com>
2024-05-20 09:40:42 +0530

    date filter in dashboard

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -19,53 +19,53 @@ export class ApiService {
     }
   }
 
-  // get(url: string): Observable<any> {
-  //   return this.http.get(`${environment.api_url}${url}`);
-  // }
-
-  // // Generic POST method
-  // post(url: string, data: any): Observable<any> {
-  //   return this.http.post(`${environment.api_url}${url}`, data);
-  // }
-
-  // // Generic PUT method
-  // update(url: string, data: any): Observable<any> {
-  //   return this.http.put(`${environment.api_url}${url}`, data);
-  // }
-
-  // // Generic DELETE method
-  // delete(url: string): Observable<any> {
-  //   return this.http.delete(`${environment.api_url}${url}`);
-  // }
-
-  private getHeaders(): HttpHeaders {
-    const authToken = localStorage.getItem('userToken');
-    let headers = new HttpHeaders().set('ngrok-skip-browser-warning', 'true');
-    if (authToken) {
-      headers = headers.set('Authorization', `Bearer ${authToken}`);
-    }
-    return headers;
-  }
-
-  // Generic GET method
   get(url: string): Observable<any> {
-    return this.http.get(`${environment.api_url}${url}`, { headers: this.getHeaders() });
+    return this.http.get(`${environment.api_url}${url}`);
   }
 
   // Generic POST method
   post(url: string, data: any): Observable<any> {
-    return this.http.post(`${environment.api_url}${url}`, data, { headers: this.getHeaders() });
+    return this.http.post(`${environment.api_url}${url}`, data);
   }
 
   // Generic PUT method
   update(url: string, data: any): Observable<any> {
-    return this.http.put(`${environment.api_url}${url}`, data, { headers: this.getHeaders() });
+    return this.http.put(`${environment.api_url}${url}`, data);
   }
 
   // Generic DELETE method
   delete(url: string): Observable<any> {
-    return this.http.delete(`${environment.api_url}${url}`, { headers: this.getHeaders() });
+    return this.http.delete(`${environment.api_url}${url}`);
   }
+
+  // private getHeaders(): HttpHeaders {
+  //   const authToken = localStorage.getItem('userToken');
+  //   let headers = new HttpHeaders().set('ngrok-skip-browser-warning', 'true');
+  //   if (authToken) {
+  //     headers = headers.set('Authorization', `Bearer ${authToken}`);
+  //   }
+  //   return headers;
+  // }
+
+  // // Generic GET method
+  // get(url: string): Observable<any> {
+  //   return this.http.get(`${environment.api_url}${url}`, { headers: this.getHeaders() });
+  // }
+
+  // // Generic POST method
+  // post(url: string, data: any): Observable<any> {
+  //   return this.http.post(`${environment.api_url}${url}`, data, { headers: this.getHeaders() });
+  // }
+
+  // // Generic PUT method
+  // update(url: string, data: any): Observable<any> {
+  //   return this.http.put(`${environment.api_url}${url}`, data, { headers: this.getHeaders() });
+  // }
+
+  // // Generic DELETE method
+  // delete(url: string): Observable<any> {
+  //   return this.http.delete(`${environment.api_url}${url}`, { headers: this.getHeaders() });
+  // }
 }
 
 
