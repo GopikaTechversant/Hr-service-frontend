0800480a8c4230e7afe8d11c90d7aa088020a82a
GopikaTechversant <gopikakg@techversantinfotech.com>
2024-05-27 10:53:38 +0530

    added management station

---
+++ src/app/modules/management/management.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ManagementHomeComponent } from './components/management-home/management-home.component';
+
+
+
+@NgModule({
+  declarations: [
+    ManagementHomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ManagementModule { }
