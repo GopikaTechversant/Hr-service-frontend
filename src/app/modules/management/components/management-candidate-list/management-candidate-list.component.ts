97403cf46c4c2c266be3c0aec65b09d22f8656c5
GopikaTechversant <gopikakg@techversantinfotech.com>
2024-05-28 15:22:54 +0530

    management candidate detail page

--- src/app/modules/management/components/management-candidate-list/management-candidate-list.component.ts
+++ src/app/modules/management/components/management-candidate-list/management-candidate-list.component.ts
@@ -1,10 +1,166 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ManagementDetailComponent } from '../management-detail/management-detail.component';
+import { ApiService } from 'src/app/services/api.service';
+import { MatDialog } from '@angular/material/dialog';
+import { StationSwitchComponent } from 'src/app/components/station-switch/station-switch.component';
+import { WarningBoxComponent } from 'src/app/components/warning-box/warning-box.component';
 @Component({
   selector: 'app-management-candidate-list',
   templateUrl: './management-candidate-list.component.html',
   styleUrls: ['./management-candidate-list.component.css']
 })
-export class ManagementCandidateListComponent {
+export class ManagementCandidateListComponent implements OnInit{
+  candidateList:any[]=[];
+  searchKeyword:string = '';
+  currentPage: number = 1;
+  limit: number = 10
+  filteredStatus: any = '';
+  displayPosition: string = '';
+  positionId: any;
+  requestList_open: any;
+  requestList: any;
+  initialLoader:boolean = false
+  loader: boolean = true;
+  totalCount: any;
+  lastPage: any;
+  filterStatus: boolean = false;
+  Status: any = [
+    { status: 'pending' },
+    { status: 'rejected' },
+    { status: 'done' },
+    { status: 'moved' }
+  ]
+  constructor(private apiService: ApiService,private dialog: MatDialog){
 
+  }
+
+  ngOnInit(): void {
+    this.fetchCandidates();
+  }
+
+  fetchCandidates():void{
+    if(!this.initialLoader) this.loader = true;
+    this.apiService.get(`/management-station/list?search=${this.searchKeyword}&page=${this.currentPage}&limit=${this.limit}&status_filter=${this.filteredStatus}&report=false`).subscribe((data: any) => {
+      this.candidateList = [];
+      console.log("this.candidateList ", this.candidateList );
+      this.initialLoader = false;
+      this.loader = false;
+      if (data.candidates) {
+        console.log("data.candidates",data.candidates);
+        this.candidateList.push(data.candidates);
+        this.totalCount = data?.totalCount
+        const totalPages = Math.ceil(this.totalCount / this.limit);
+        this.lastPage = totalPages;
+        if (this.currentPage > totalPages) this.currentPage = totalPages;
+      }
+    })
+  }
+
+  fetchDetails(id: any, status: any): void {
+    this.apiService.get(`/management-station/candidateDetail?serviceId=${id}`).subscribe((data: any) => {
+      if (data?.candidates) this.viewCandidateDetail(data?.candidates, status);
+    });
+  }
+
+  viewCandidateDetail(item: any, status: any): void {
+    console.log("open modal");
+    
+    const dialogRef = this.dialog.open(ManagementDetailComponent, {
+      // data: { candidateDetails: item, offerStatus: status },
+    })
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      this.fetchCandidates();
+    })
+  }
+
+  searchCandidate(searchTerm: string): void {
+    this.searchKeyword = searchTerm;
+    this.currentPage = 1;
+    this.limit = 10;
+    this.fetchCandidates();
+  }
+
+  clearFilter(item: any): void {
+    if (item === 'status') {
+      this.filteredStatus = '';
+      sessionStorage.setItem('status', this.filteredStatus);
+    }
+    if (item === 'position') {
+      this.displayPosition = '';
+      this.positionId = '';
+      sessionStorage.setItem(`requirement`, JSON.stringify({ name: this.displayPosition, id: this.positionId }));
+    }
+    if (item === 'search') this.searchKeyword = '';
+    this.currentPage = 1;
+    this.limit = 10;
+    this.fetchCandidates();
+  }
+
+  selectPosition(name: string, id: string): void {
+    console.log("selectPosition");
+    
+    this.requestList_open = false;
+    this.displayPosition = name;
+    this.positionId = id;
+    console.log("this.positionId",this.positionId);
+    
+    sessionStorage.setItem(`requirement`, JSON.stringify({ name: this.displayPosition, id: this.positionId }));
+    this.fetchCandidates();
+  }
+
+  onSwitchStation(candidate: any): void {
+    if (candidate?.serviceStatus === 'pending' && candidate?.progressStatus === '0') {
+      const userId = localStorage.getItem('userId');
+      const dialogRef = this.dialog.open(StationSwitchComponent, {
+        data: {
+          userId: userId,
+          name: candidate['candidate.candidateFirstName'] + ' ' + candidate['candidate.candidateLastName'],
+          serviceId: candidate?.serviceId,
+          currentStation: 'Hr Manager',
+          currentStationId: '5',
+          requirement: candidate['serviceRequest.requestName']
+        },
+      })
+      dialogRef.afterClosed().subscribe(() => {
+        this.fetchCandidates();
+      });
+    } else {
+      const dialogRef = this.dialog.open(WarningBoxComponent, {})
+      dialogRef.afterClosed().subscribe(() => {
+        this.fetchCandidates();
+      });
+    }
+  }
+
+  selectStatusFilter(item: string): void {
+    this.filteredStatus = item;
+    sessionStorage.setItem('status', this.filteredStatus);
+    this.currentPage = 1;
+    this.limit = 10;
+    this.fetchCandidates();
+  }
+
+  onPageChange(pageNumber: number): void {
+    this.currentPage = Math.max(1, pageNumber);
+    this.fetchCandidates();
+  }
+
+  generatePageNumbers() {
+    let pages = [];
+    if (this.lastPage <= 5) {
+      for (let i = 1; i <= this.lastPage; i++) pages.push(i);
+    } else {
+      pages.push(1);
+      let start = Math.max(2, this.currentPage - 1);
+      let end = Math.min(this.lastPage - 1, this.currentPage + 1);
+
+      if (this.currentPage <= 3) end = 4;
+      else if (this.currentPage >= this.lastPage - 2) start = this.lastPage - 3;
+      if (start > 2) pages.push('...');
+      for (let i = start; i <= end; i++) pages.push(i);
+      if (end < this.lastPage - 1) pages.push('...');
+      pages.push(this.lastPage);
+    }
+    return pages;
+  }
 }
