579f4aae8ebe90624b6c0066b31882304c8fe0fa
Alfiya S R <alfiya.sr@techversantinfotech.com>
2024-06-14 18:01:57 +0530

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -22,6 +22,12 @@ export class HeaderComponent implements OnInit {
   showCandidates: boolean = false;
   stationsList: any[] = [];
   headers: any;
+  profileDropDown: any = [{ stationName: 'Users' }, { stationName: 'Log Out' }];
+  isSidebarHidden: boolean = false;
+  currentRole: any = '';
+  activeStation: any;
+  stationId: any;
+  url: any;
   constructor(private apiService: ApiService, private dialog: MatDialog, private router: Router, private el: ElementRef, private route: ActivatedRoute) { }
 
   @HostListener('document:click', ['$event'])
@@ -34,27 +40,39 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.currentUser = localStorage.getItem('userRole');
+    const currentStation = this.router.url.split('/')[1];
+    this.route.params.subscribe(params => {
+      this.stationId = params['id'];
+      this.url = `/technical/${this.stationId}`;
+    });
+    if(currentStation === 'dashboard') this.activeStation = 'Screening';
+    if(currentStation === 'written') this.activeStation = 'Written';
+    if(currentStation === 'technical' && this.stationId === '3') this.activeStation = 'Technical 1';
+    if(currentStation === 'technical' && this.stationId === '4') this.activeStation = 'Technical 2';
+    if(currentStation === 'hr') this.activeStation = 'Hr Manager';
+    if(currentStation === 'management') this.activeStation = 'Management';
+
+    this.currentRole = localStorage.getItem('userRole');
+    this.currentUser = localStorage.getItem('userFullName');
     this.fetchStations();
   }
 
   fetchStations(): void {
     this.apiService.get(`/user/stations`).subscribe((res: any) => {
       this.stationsList = res?.data;
-      this.stationsList.push({
-        "stationName": "Log Out",
-        "stationId": 0
-      })
     });
   }
 
   navigate(station: any): void {
+    console.log(this.activeStation == station?.stationName);
+
     if (station?.stationName === 'Technical 1') this.router.navigate([`/technical/${station?.stationId}`]);
     else if (station?.stationName === 'Technical 2') this.router.navigate([`/technical/${station?.stationId}`]);
     else if (station?.stationName === 'Hr Manager') this.router.navigate(['/hr']);
     else if (station?.stationName === 'Screening') this.router.navigate(['/dashboard']);
     else if (station?.stationName === 'Written') this.router.navigate(['/written']);
     else if (station?.stationName === 'Management') this.router.navigate(['/management']);
+    else if (station?.stationName === 'Users') this.router.navigate(['/user']);
     else if (station?.stationName === 'Log Out') this.logout();
     this.dropDown = false;
   }
@@ -67,12 +85,16 @@ export class HeaderComponent implements OnInit {
         if (this.candidateList?.length > 0) this.showCandidates = true;
       }
     })
-    if(this.searchKeyword.trim() === '') this.showCandidates = false;
+    if (this.searchKeyword.trim() === '') this.showCandidates = false;
   }
 
   selectCandidate(id: any): void {
     this.router.navigate([`candidate-details`, id], { relativeTo: this.route });
     this.searchKeyword = '';
+  }
+
+  toggleSidebar() {
+    this.isSidebarHidden = !this.isSidebarHidden;
   }
 
   profileClick() {
