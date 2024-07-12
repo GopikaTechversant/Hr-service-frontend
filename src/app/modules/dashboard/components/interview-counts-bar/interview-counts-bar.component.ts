c218ae87387490b01dc5a68091576ce7d5de3d3d
Alfiya S R <alfiya.sr@techversantinfotech.com>
2024-07-12 17:26:48 +0530

    interview count chart

--- src/app/modules/dashboard/components/interview-counts-bar/interview-counts-bar.component.ts
+++ src/app/modules/dashboard/components/interview-counts-bar/interview-counts-bar.component.ts
@@ -15,11 +15,12 @@ export class InterviewCountsBarComponent implements OnInit {
   chart: any;
   sixMonthCount: any;
   labels: any;
-  dataSet: any;
+  dataSets: any[] = [];
   teamDetails: any;
   showDepartment: boolean = false;
   teamId: any = '';
   teamName: string = '';
+
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
@@ -38,20 +39,30 @@ export class InterviewCountsBarComponent implements OnInit {
   fetchBarchartDetails(): void {
     this.apiService.get(`/dashboard/six-month-count?team=${this.teamId}`).subscribe((res: any) => {
       if (res?.data) {
-        this.sixMonthCount = res?.data;
-        this.labels = Object.keys(this.sixMonthCount[0]);
-        this.dataSet = Object.values(this.sixMonthCount[0]);
+        this.sixMonthCount = res.data;
+        // http://localhost:3001/dashboard/recruiter-chart?end_date=2024-08-01&start_date=2024-06-01
+        //   this.labels = Object.keys(this.sixMonthCount[0]);
+        //   this.dataSets = this.sixMonthCount.map((item: any, index: number) => ({
+        //     label: `Series ${index + 1}`, // Adjust the label as needed
+        //     data: Object.values(item),
+        //     backgroundColor: `rgba(98, 138, 252, ${0.6 + index * 0.1})`,
+        //     borderColor: `rgba(98, 138, 252, ${0.6 + index * 0.1})`,
+        //     borderWidth: 1,
+        //     barThickness: 40,
+        //     barPercentage: 1,
+        //     categoryPercentage: 0.9,
+        //   }));
         this.createBarChart();
       }
-    })
+    });
   }
 
   fetchDepartment(): void {
     this.apiService.get(`/service-request/team`).subscribe((res: any) => {
       if (res?.data) {
-        this.teamDetails = res?.data
+        this.teamDetails = res.data;
       }
-    })
+    });
   }
 
   selectDepartment(team: string, teamId: string): void {
@@ -62,36 +73,57 @@ export class InterviewCountsBarComponent implements OnInit {
   }
 
   clearFilter(): void {
-    this.teamName = "";
+    this.teamName = '';
     this.fetchBarchartDetails();
   }
 
-  createBarChart() {
+  createBarChart(): void {
     if (this.chart) this.chart.destroy();
     this.chart = new Chart('barChartInterview', {
       type: 'bar',
       data: {
-        labels: this.labels,
-        datasets: [{
-          label: 'Counts',
-          data: this.dataSet,
-          backgroundColor: 'rgba(98, 138, 252)',
-          borderColor: 'rgba(98, 138, 252)',
-          borderWidth: 1,
-          barThickness: 40,
-          barPercentage: 1,
-          categoryPercentage: 0.9,
-        }]
+        labels: ['Amritha', 'Jiji', 'Maloo', 'Parvathy'],
+        datasets: [
+          {
+            label: 'No. of Hired',
+            data: [4, 4, 2, 0], // Adjust data values accordingly
+            backgroundColor: '#047892',
+            borderColor: '#047892',
+            borderWidth: 1,
+            barPercentage: 0.8,
+            categoryPercentage: 0.7,
+          },
+          {
+            label: 'No. of Sourced',
+            data: [12, 13, 11, 14], // Adjust data values accordingly
+            backgroundColor: '#628AFC',
+            borderColor: '#628AFC',
+            borderWidth: 1,
+            barPercentage: 0.8,
+            categoryPercentage: 0.7,
+          },
+          {
+            label: 'No. of Offered',
+            data: [5, 4, 3, 1], // Adjust data values accordingly
+            backgroundColor: '#005EC9',
+            borderColor: '#005EC9',
+            borderWidth: 1,
+            barPercentage: 0.8,
+            categoryPercentage: 0.7,
+          },
+
+        ]
       },
       options: {
         scales: {
           y: {
             beginAtZero: true,
-            grid: {
-              display: false,
+            ticks: {
+             
             }
           },
           x: {
+            beginAtZero: false,
             grid: {
               display: false,
             }
@@ -99,7 +131,7 @@ export class InterviewCountsBarComponent implements OnInit {
         },
         layout: {
           padding: {
-            top: 30,
+            top: 10,
             right: 10,
             bottom: 10,
             left: 10
@@ -107,31 +139,35 @@ export class InterviewCountsBarComponent implements OnInit {
         },
         plugins: {
           legend: {
-            display: false
+            display: true,
+            // position: 'right',
+            // align: 'end',
+            labels: {
+              usePointStyle: true,
+              pointStyle: 'circle',
+              padding: 20,
+            }
           },
           tooltip: {
             enabled: true,
           },
           datalabels: {
-            color: '#0034C4',
-            backgroundColor: '#FFFFFF',
-            borderRadius: 4,
-            borderColor: '#0034C4',
-            borderWidth: 1,
+            color: '#575F6E',
+            // backgroundColor: '#FFFFFF',
+            // borderRadius: 4,
+            // borderColor: '#0034C4',
+            // borderWidth: 1,
             padding: 4,
             anchor: 'end',
             align: 'end',
             offset: -1,
             font: {
-              size: 11,
+              size: 14,
               weight: 400,
             },
-            formatter: (value: any, context: any) => {
-              if (context.chart && context.chart.data.labels && context.dataIndex < context.chart.data.labels.length) {
-                return value + ' Interviews';
-              }
-              return 'Unknown: ' + value;
-            },
+            // formatter: (value: any, context: any) => {
+            //   return value + ' Interviews';
+            // },
           },
         }
       },
